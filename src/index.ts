acafe4f533530525b3306c9cf0d2ae941d3ecb62
Erik Knaap <erik_knaap@hotmail.com>
2023-12-11 11:14:06 +0100

    DataBase dump toegevoegd

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,76 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import { Sequelize, DataTypes } from 'sequelize';
+//import { Employees, PayChecks } from './models';
+
+const sequelize = new Sequelize('SMARTQA', 'erik', '57625762', {
+    host: 'localhost',
+    dialect: 'mariadb'
+});
+
+try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.')
+} catch(error) {
+    console.error('Unable to connect to the database: ', error);
+}
+
+const Employee = sequelize.define('Employee', {
+    ID: {
+        type: DataTypes.NUMBER,
+        allowNull: false,
+    },
+    FirstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    LastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    No: {
+        type: DataTypes.NUMBER,
+        allowNull: false,
+        
+    }
+}, {
+    timestamps: false,
+});
+
+//console.log(Employee === sequelize.models.Employee);
+
+const PayCheck = sequelize.define('PayCheck', {
+    ID: {
+        type: DataTypes.NUMBER,
+        allowNull: false,
+    },
+    EmployeeNo: {
+        type: DataTypes.NUMBER,
+        allowNull: false,
+    },
+    Month: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    Year: {
+        type: DataTypes.NUMBER,
+        allowNull: false,
+    },
+    GrossAmount: {
+        type: DataTypes.NUMBER,
+        allowNull: false,
+    },
+    NettAmount: {
+        type: DataTypes.NUMBER,
+        allowNull: false,
+    },
+    PDF: {
+        type: DataTypes.BLOB,
+        allowNull: true,
+    }
+}, {
+    timestamps: false,
+});
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
@@ -9,17 +80,17 @@ const typeDefs = `#graphql
 
   # This "Employee" type defines the queryable fields for every employee in our data source.
   type Employee {
-    firstName: String!
-    lastName: String!
-    employeeNo: Int!
+    FirstName: String!
+    LastName: String!
+    No: Int!
     payChecks: [PayCheck]
   }
   type PayCheck {
-    month: Int!
-    year: Int!
-    grossAmount: Float!
-    nettAmount: Float!
-    pdf: String
+    Month: Int!
+    Year: Int!
+    GrossAmount: Float!
+    NettAmount: Float!
+    PDF: String
   }
 
   # The "Query" type is special: it lists all of the available queries that
@@ -32,87 +103,30 @@ const typeDefs = `#graphql
   }
 `;
 
-const employees = [
-    {
-        employeeNo: 1,
-        firstName: "Ico",
-        lastName: "Bakker",
-        payChecks: [
-            {
-                month: 2,
-                year: 2023,
-                grossAmount: 4556.55,
-                nettAmount: 3241.51
-            }
-        ]
-    }, 
-    {
-        employeeNo: 2,
-        firstName: "Erik",
-        lastName: "Knaap",
-        payChecks: [
-            {
-                month: 2,
-                year: 2023,
-                grossAmount: 5645.14,
-                nettAmount: 4106.66
-            }
-        ]
-    },
-    {
-        employeeNo: 3,
-        firstName: "Akash",
-        lastName: "Foederer",
-        payChecks: [
-            {
-                month: 2,
-                year: 2023,
-                grossAmount: 4104.55,
-                nettAmount: 2911.75
-            }
-        ]
-    }
-]
-
 const resolvers = {
     Query: {
-        employees: () => employees,
-        employee: (_, { employeeNo } ) => {
-            const employee = employees.find(e => e.employeeNo == employeeNo)
+        employees: () => Employee.findAll(),
+        employee: async (_, { employeeNo } ) => {
+            const employee = await Employee.findOne({
+                where: {
+                    No: employeeNo
+                }
+            })
+            employee['payChecks'] = PayCheck.findAll({ where: {
+                employeeNo: employee['No']
+            }});
             return employee
         },
         payChecks: (_, { employeeNo }) => {
-            return employees.find(e => e.employeeNo == employeeNo).payChecks
+            return PayCheck.findOne({
+                where: {
+                    employeeNo: employeeNo
+                }
+            })
         }
     }
 }
 
-// import { GraphQLScalarType, Kind } from 'graphql';
-
-// const dateScalar = new GraphQLScalarType({
-//   name: 'Date',
-//   description: 'Date custom scalar type',
-//   serialize(value) {
-//     if (value instanceof Date) {
-//       return value.getTime(); // Convert outgoing Date to integer for JSON
-//     }
-//     throw Error('GraphQL Date Scalar serializer expected a `Date` object');
-//   },
-//   parseValue(value) {
-//     if (typeof value === 'number') {
-//       return new Date(value); // Convert incoming integer to Date
-//     }
-//     throw new Error('GraphQL Date Scalar parser expected a `number`');
-//   },
-//   parseLiteral(ast) {
-//     if (ast.kind === Kind.INT) {
-//       // Convert hard-coded AST string to integer and then to Date
-//       return new Date(parseInt(ast.value, 10));
-//     }
-//     // Invalid hard-coded value (not an integer)
-//     return null;
-//   },
-// });
 
 const server = new ApolloServer({
     typeDefs,
---
+++ src/models.ts
@@ -0,0 +1,22 @@
+/*
+import { Model, InferAttributes, InferCreationAttributes, CreationOptional, NumberDataType } from 'sequelize';
+
+class Employees extends Model<InferAttributes<Employees>, InferCreationAttributes<Employees>> {
+    declare ID: number;
+    declare FirstName: string;
+    declare LastName: string;
+    declare No: number;
+}
+
+class PayChecks extends Model<InferAttributes<PayChecks>, InferCreationAttributes<PayChecks>> {
+    declare ID: number;
+    declare Employees_ID: number;
+    declare Month: number;
+    declare Year: number;
+    declare GrossAmount: number;
+    declare NettAmount: number;
+    declare PDF: Blob
+}
+
+export { Employees, PayChecks }
+*/
